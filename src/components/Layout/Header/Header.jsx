5eafe9a91bedc7b710219e06ea104ccac48d879d
MatiBross <matisotelo2004@gmail.com>
2026-05-21 18:20:48 -0300

    feat: update Header component to use NavLink for navigation and enhance active link styling

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,8 +1,9 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import CartWidget from "./CartWidget";
 
 export default function Header() {
-  const linkClass = "hover:text-accent-primary transition-all duration-300";
+  const linkClass = ({isActive}) =>`${isActive ? "text-accent-primary font-bold " :"hover:text-accent-primary transition-all duration-300"}`;
+  
 
   return (
     <header className="bg-primary flex gap-3 flex-col justify-between items-center p-6 sticky top-0 z-50 md:flex-row">
@@ -15,19 +16,19 @@ export default function Header() {
       <nav className="text-text-secondary flex font-sans">
         <ul className="flex gap-10">
           <li>
-            <Link to={"/"} className={linkClass}>
+            <NavLink to={"/"} className={linkClass}>
               Inicio
-            </Link>
+            </NavLink>
           </li>
           <li>
-            <Link to={"/productos"} className={linkClass}>
+            <NavLink to={"/productos"} className={linkClass}>
               Productos
-            </Link>
+            </NavLink>
           </li>
           <li>
-            <Link to={"/sobre-nosotros"} className={linkClass}>
+            <NavLink to={"/sobre-nosotros"} className={linkClass}>
               Sobre Nosotros
-            </Link>
+            </NavLink>
           </li>
         </ul>
       </nav>
